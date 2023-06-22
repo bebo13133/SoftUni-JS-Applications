1a932a1420de7001b638f05857048a93db9ccef5
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-06-22 11:46:54 +0300

    add

--- 02. JS-Applications-Asynchronous-Programming-Lab-Resources/03.Gighub-Commits/app.js
+++ 02. JS-Applications-Asynchronous-Programming-Lab-Resources/03.Gighub-Commits/app.js
@@ -1,4 +1,27 @@
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+
+    const [userName, repo,commits] = ['#username', "#repo", '#commits']
+    .map(selector=>document.querySelector(selector));
+
+    let url ='https://api.github.com/repos'
+    fetch(`${url}/${userName.value}/${repo.value}/commits`)
+
+    .then(response =>{
+        commits.innerHTML= "";
+        if(!response.ok) throw new Error(`${response.status}`)
+        return response.json()
+})
+.then(data =>{
+commits.innerHTML='';
+data.forEach(e => {
+  let li = document.createElement('li')
+  li.textContent = `${e.commit.author.name}: ${e.commit.message}`  
+commits.appendChild(li)
+});
+
+})
+.catch(err=>{
+    commits.textContent = err.message
+})
+
 }
