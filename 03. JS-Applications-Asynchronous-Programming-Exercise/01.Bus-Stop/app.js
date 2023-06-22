be46bb5eed30078c932758d81ea2929eec554413
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-06-22 13:47:36 +0300

    add

--- 03. JS-Applications-Asynchronous-Programming-Exercise/01.Bus-Stop/app.js
+++ 03. JS-Applications-Asynchronous-Programming-Exercise/01.Bus-Stop/app.js
@@ -1,3 +1,33 @@
-function getInfo() {
-    console.log("TODO...");
+function getInfo(e) {
+
+    const [stopId, submit, result, buses, stopName] = ['#stopId', "#submit", "#result", '#buses', '#stopName']
+        .map(selector => document.querySelector(selector));
+    const url = 'http://localhost:3030/jsonstore/bus/businfo'
+
+
+    buses.innerHTML = "";
+    stopName.innerHTML = "loading..."
+    fetch(`${url}/${stopId.value}`)
+        .then(response => {
+            if (response.status !== 200 || submit.value == "") {
+                throw new Error();
+            }
+
+            return response.json()
+        })
+        .then((data) => {
+
+            Object.entries(data.buses).forEach((e) => {
+                let li = document.createElement('li')
+                li.textContent = `Bus ${e[0]} arrives in ${e[1]} minutes`
+                buses.appendChild(li)
+
+            });
+            stopName.textContent = data.name
+        })
+        .catch((err) => {
+            stopName.textContent = `${err}`
+        })
+
+
 }
