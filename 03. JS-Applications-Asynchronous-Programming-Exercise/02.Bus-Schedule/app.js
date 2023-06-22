ebac1023478633c9e776e7e1a1a1f5ec4d12d50d
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-06-22 15:18:27 +0300

    add

--- 03. JS-Applications-Asynchronous-Programming-Exercise/02.Bus-Schedule/app.js
+++ 03. JS-Applications-Asynchronous-Programming-Exercise/02.Bus-Schedule/app.js
@@ -1,11 +1,36 @@
 function solve() {
+    const [info, departBtn, arriveBtn] = ["#info span", "#depart",'#arrive']
+    .map(selector=>document.querySelector(selector));
 
-    function depart() {
-        console.log('Depart TODO...');
+    let stop = {
+        next:'depot'
     }
 
+    async function depart() {
+        departBtn.disabled = true;
+
+
+
+        const url =`http://localhost:3030/jsonstore/bus/schedule/${stop.next}`
+        const response = await fetch(url)
+        if(response.status !== 200){
+            departBtn.disabled  =true
+            arriveBtn.disabled = true
+            throw new Error("Error")
+
+        }
+   stop = await response.json()
+
+
+    info.textContent =`Next stop ${stop.name}`
+
+arriveBtn.disabled = false 
+}
+
     function arrive() {
-        console.log('Arrive TODO...');
+   departBtn.disabled = false
+   info.textContent = `Arriving at ${stop.name}`
+   arriveBtn.disabled=true
     }
 
     return {
