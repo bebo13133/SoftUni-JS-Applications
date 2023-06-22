249a32784388182569f0a572ab5b51fee2906554
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-06-22 19:13:48 +0300

    add

--- 03. JS-Applications-Asynchronous-Programming-Exercise/02.Bus-Schedule/app.js
+++ 03. JS-Applications-Asynchronous-Programming-Exercise/02.Bus-Schedule/app.js
@@ -1,36 +1,30 @@
 function solve() {
-    const [info, departBtn, arriveBtn] = ["#info span", "#depart",'#arrive']
-    .map(selector=>document.querySelector(selector));
+    const [info, departBtn, arriveBtn] = ["#info span", "#depart", '#arrive']
+        .map(selector => document.querySelector(selector));
 
-    let stop = {
-        next:'depot'
+    let stations = {
+        next: 'depot'
     }
-
     async function depart() {
         departBtn.disabled = true;
-
-
-
-        const url =`http://localhost:3030/jsonstore/bus/schedule/${stop.next}`
+        const url = `http://localhost:3030/jsonstore/bus/schedule/${stations.next}`
         const response = await fetch(url)
-        if(response.status !== 200){
-            departBtn.disabled  =true
+        if (response.status !== 200) {
+            departBtn.disabled = true
             arriveBtn.disabled = true
             throw new Error("Error")
 
         }
-   stop = await response.json()
+        stations = await response.json()
+        info.textContent = `Next stop ${stations.name}`
 
-
-    info.textContent =`Next stop ${stop.name}`
-
-arriveBtn.disabled = false 
-}
+        arriveBtn.disabled = false
+    }
 
     function arrive() {
-   departBtn.disabled = false
-   info.textContent = `Arriving at ${stop.name}`
-   arriveBtn.disabled=true
+        departBtn.disabled = false
+        info.textContent = `Arriving at ${stations.name}`
+        arriveBtn.disabled = true
     }
 
     return {
--- 03. JS-Applications-Asynchronous-Programming-Exercise/05. Accordion/accordion.js
+++ 03. JS-Applications-Asynchronous-Programming-Exercise/05. Accordion/accordion.js
@@ -1,3 +1,50 @@
-function solution() {
-    //TODO .....
+async function solution() {
+const main = document.getElementById('main')
+const url = `http://localhost:3030/jsonstore/advanced/articles/list`
+
+const response = await fetch(url)
+const data = await response.json()
+data.forEach(e => {
+    const divAcordion = createElement('div','',"accordion")
+    const divHead = createElement('div','','head')
+    const span = createElement('span',e.title)
+    const moreButton = createElement("button",'More','button',e._id)
+    const extra = createElement('div','', 'extra')
+    let p = createElement('p')
+   
+    moreButton.addEventListener('click',onToggle)
+    divAcordion.appendChild(divHead)
+    divHead.appendChild(span)
+    divHead.appendChild(moreButton)
+    divAcordion.appendChild(extra)
+    extra.appendChild(p)
+    main.appendChild(divAcordion)
+    
+});
+
+async function onToggle(e){
+let accordion = e.target.parentElement.parentElement
+const url = `http://localhost:3030/jsonstore/advanced/articles/details/${e.target.id}`
+const extra = accordion.querySelector('.extra')
+const res = await fetch(url)
+const dataRes = await res.json()
+const hidden = e.target.textContent ==='More'
+
+const pElem = accordion.querySelector("p")
+pElem.textContent = dataRes.content
+
+extra.style.display = hidden ? "block": 'none'
+e.target.textContent = hidden? 'Less' : 'More'
+
+
 }
+
+    function createElement(type, content, className, idName) {
+        let element = document.createElement(type);
+        content ? element.textContent = content : '';
+        className ? element.classList.add(className) : '';
+        idName ? element.id = idName : '';
+        return element;
+    }
+}
+solution()
