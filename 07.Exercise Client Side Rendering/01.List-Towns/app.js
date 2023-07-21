a9480757bb49e79fd109c9fb24c9270022aca886
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-07-21 21:12:26 +0300

    up

--- 07.Exercise Client Side Rendering/01.List-Towns/app.js
+++ 07.Exercise Client Side Rendering/01.List-Towns/app.js
@@ -1,31 +1,18 @@
-import { html,render } from "./node_modules/lit-html/lit-html.js"
+import { html, render } from "./node_modules/lit-html/lit-html.js";
 
-document.querySelector('#btnLoadTowns').addEventListener('click',(e)=>{
-e.preventDefault()
-const input = document.querySelector('#towns').value
-const towns = input.split(", ")
+const [inputField, loadBtn, root] = ['#towns', '#btnLoadTowns', '#root'].map(selector => document.querySelector(selector))
+loadBtn.addEventListener('click', onLoad);
+const listTemplate = (data) => html`
+<ul> 
+${data.map(town => html`<li>${town}</li>`)}
+</ul>
+`
+function onLoad(e) {
+  e.preventDefault()
+  if(inputField.value !=''){
+    const towns = inputField.value.split(', ')
+    render(listTemplate(towns), root)
+    inputField.value = ''
+  }
 
-const template =html`
-  <ul>
-    ${towns.map((t) => html`<li>${t}</li>`)}
-  </ul>
-`;
-const rootElement= document.querySelector("#root")
-render(template,rootElement)
-
-
-})
-
-
-
-
-
-const container = document.querySelector("#root");
-
-document.querySelector(".content").addEventListener("submit", (e) => {
-  e.preventDefault();
-  let data = new FormData(e.currentTarget);
-  let towns = data.get("towns").split(", ");
-
-  render(template(towns), container);
-})
+}
