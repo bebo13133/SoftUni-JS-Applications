4dd59fb5c2bb7eb5f33991989a943fb2c47771c9
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-07-22 17:54:59 +0300

    up

--- 07.Exercise Client Side Rendering/04.Fill-Dropdown/dropdown.js
+++ 07.Exercise Client Side Rendering/04.Fill-Dropdown/dropdown.js
@@ -1,3 +1,31 @@
-function addItem() {
-    console.log('TODO:...');
+import {html, render} from './node_modules/lit-html/lit-html.js'
+const form = document.querySelector('form')
+form.addEventListener('submit', onCheck)
+const url = `http://localhost:3030/jsonstore/advanced/dropdown`
+const menu = document.querySelector('#menu')
+async function getTown(){
+    const response = await fetch(url)
+    const data =await response.json()
+    return Object.values(data)
+}
+const townTemplate = (data) =>html`
+${data.map(town=>html`<option value=${town._id}>${town.text}</option>`)}`
+render(townTemplate(await getTown()),menu)
+function onCheck(e){
+    e.preventDefault()
+    const formData = new FormData(e.target)
+    addItem([...formData.entries()].reduce((p, [k, v]) => Object.assign(p, { [k]: v }), {}));
+}
+async function addItem(info) {
+   const body ={
+    text: info.text 
+   }
+    const response = await fetch(url,{
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(body)
+    })
+ const data = await response.json()
+ form.reset()
+ render(townTemplate(await getTown()),menu)
 }
