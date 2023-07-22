7de09ea8e62323fba2bb5559929c091e60412ead
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-07-22 17:55:53 +0300

    Update dropdown.js

--- 07.Exercise Client Side Rendering/04.Fill-Dropdown/dropdown.js
+++ 07.Exercise Client Side Rendering/04.Fill-Dropdown/dropdown.js
@@ -1,16 +1,14 @@
-import {html, render} from './node_modules/lit-html/lit-html.js'
-const form = document.querySelector('form')
-form.addEventListener('submit', onCheck)
-const url = `http://localhost:3030/jsonstore/advanced/dropdown`
-const menu = document.querySelector('#menu')
+import {html, render} from './node_modules/lit-html/lit-html.js';
+const form = document.querySelector('form');
+form.addEventListener('submit', onCheck);
+const url = `http://localhost:3030/jsonstore/advanced/dropdown`;
 async function getTown(){
     const response = await fetch(url)
-    const data =await response.json()
-    return Object.values(data)
+    return Object.values(await response.json())
 }
 const townTemplate = (data) =>html`
 ${data.map(town=>html`<option value=${town._id}>${town.text}</option>`)}`
-render(townTemplate(await getTown()),menu)
+render(townTemplate(await getTown()),document.querySelector('#menu'))
 function onCheck(e){
     e.preventDefault()
     const formData = new FormData(e.target)
