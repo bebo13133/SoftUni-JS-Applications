51c0fd337791a66b5ea033e41aaee8dbe2b319d2
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-07-22 11:31:14 +0300

    up

--- 07.Exercise Client Side Rendering/02.HTTP-Status-Cats/app.js
+++ 07.Exercise Client Side Rendering/02.HTTP-Status-Cats/app.js
@@ -1,35 +1,28 @@
-import { html, render } from './node_modules/lit-html/lit-html.js';
-import { cats } from './catSeeder.js';
+import { html, render } from './node_modules/lit-html/lit-html.js'
+import { cats } from './catSeeder.js'
+import{styleMap} from './node_modules/lit-html/directives/style-map.js'
+cats.forEach(c => c.action = "Show")
+const section = document.getElementById('allCats')
 
-const catCardTemplate = (cat) => html`
-<li>
-    <img src="./images/${cat.imageLocation}.jpg" width="250" height="250" alt="Card image cap">
-    <div class="info">
-        <button class="showBtn">Show status code</button>
-        <div class="status" style="display: none" id="${cat.id}">
-            <h4>Status Code: ${cat.statusCode}</h4>
-            <p>${cat.statusMessage}</p>
-        </div>
-    </div>
-</li>`;
-
-const main = document.getElementById('allCats');
-
-const catsList = html`<ul @click=${toggleInfo}>${cats.map(catCardTemplate)}</ul>`;
-
-
-
-render(catsList, main);
+const catTemplate = (data) => html`<ul>
+${data.map((cat) => html`
+   <li>
+            <img src="./images/${cat.imageLocation}.jpg" width="250" height="250" alt="Card image cap">
+            <div class="info">
+                    <button class="showBtn" @click=${(e) => onShow(e, cat)}>${cat.action} status code</button>
+                   <div class="status" style="${styleMap({display: cat.action == 'Show'? 'none':'block'})}" id="${cat.id}">
+                       <h4 class="card-title">Status Code: ${cat.statusCode}</h4>
+                       <p class="card-text">${cat.statusMessage}</p>
+                   </div>
+            </div>
+    </li>
+`)}
+</ul>
+`
 
 
-function toggleInfo(event) {
-    if (event.target.tagName == 'BUTTON') {
-        const element = event.target.parentNode.querySelector('.status');
-        if (element.style.display == 'none') {
-            element.removeAttribute('style');
-        } else {
-            element.style.display = 'none';
-        }
-    }
-
+function onShow(e, data) {
+data.action = data.action=="Show"? "Hide":"Show"
+render(catTemplate(cats), section)
 }
+render(catTemplate(cats), section)
